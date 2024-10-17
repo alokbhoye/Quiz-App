ebe33f6d35566de2bfa382c31eac3dfdc14380eb
alokbhoye <alokbhoye21@gmail.com>
2024-10-17 19:12:01 +0530

    BG color

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,15 @@ function App() {
 
   return (
     <>
-<div className="flex items-center justify-center">
-  <img 
-    className="w-[291px] h-[70px] mt-[40px]" 
-    src={logo} 
-    alt="Logo"
-/>
-</div>  
+    <div className=' bg-purple-300 h-screen'>
+    <div className="flex items-center justify-center">
+      <img 
+        className="w-[291px] h-[70px] mt-[40px]" 
+        src={logo} 
+        alt="Logo"
+    />
+    </div> 
+</div> 
  </>
   )
 }
