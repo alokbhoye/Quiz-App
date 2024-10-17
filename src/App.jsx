54e273ca942ff6a81563d888192824f414b62bd1
alokbhoye <alokbhoye21@gmail.com>
2024-10-17 19:05:11 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import logo from '../public/Frame.png'
+
+function App() {
+
+  return (
+    <>
+<div className="flex items-center justify-center">
+  <img 
+    className="w-[291px] h-[70px] mt-[40px]" 
+    src={logo} 
+    alt="Logo"
+/>
+</div>  
+ </>
+  )
+}
+
+export default App
